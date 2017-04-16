c9ce418b5e593583e5a4dd020f4f169802a19007
Leiming Yu <leimingyu830@gmail.com>
2017-04-16 16:09:29 -0400

    audio plot

---
+++ audio_utils.py
@@ -0,0 +1,27 @@
+import pyaudio
+import wave
+import sys
+
+
+#
+# http://people.csail.mit.edu/hubert/pyaudio/
+#
+def play_wav(wavfile):
+    CHUNK = 1024
+    wf = wave.open(wavfile, 'rb')
+    p = pyaudio.PyAudio()
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True)
+
+    data = wf.readframes(CHUNK)
+
+    while data:
+        stream.write(data)
+        data = wf.readframes(CHUNK)
+    
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
